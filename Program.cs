35f203c4d2b5650eece33dfbda87fc7525d2fe68
VusalAhmed <94452551+VusalAhmed@users.noreply.github.com>
2021-11-30 02:44:29 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace vusal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int reqem = 582;
+            int say;
+            int toplam = 0;
+            while (reqem > 0)
+            {
+                say = reqem % 10;
+                toplam = say + toplam;
+                reqem = reqem / 10;
+            }
+            Console.WriteLine(toplam);
+
+        }
+    }
+}
+  
+
+
+
+
+int season = 6;
+    switch (season)
+    {
+    case 1:
+    case 2:
+            Console.WriteLine(yaz);
+            break:
+        case 3:
+    case 4:
+        Console.WriteLine(yay);
+        break:
+        case 5:
+        Console.WiteLine(payiz);
+        break:
+        case 6:
+        Console.WriteLine(qis);
+    default:
+        Console.WriteLine("duzgun daxil ediniz");
+        break;
+    }
+
+
+
+
+
+
+int = eded;
+int i;
+if (eded=0)
+{ Console.WriteLine("ne sadedir nede murekkebdir"); }
+else if (eded < 0)
+{ Console.WriteLine("musbet eded daxil edin"); }
+else
+{
+    while(i<eded)
+    {
+        if (eded % i ==0)
+        {
+            bolen++;
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+if (bolen >2)
+{ Console.WriteLine("murekkeb ededdir"); }
+else
+{
+    Console.WriteLine("sade ededdir");
+}
