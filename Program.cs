21058db8ba7ec7c154cc2f796330b816b67ed979
VusalAhmed <94452551+VusalAhmed@users.noreply.github.com>
2021-12-03 13:54:27 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,80 +1,22 @@
 ﻿using System;
 
-namespace vusal
+namespace Employee
 {
     class Program
     {
         static void Main(string[] args)
+         
         {
-            int reqem = 582;
-            int say;
-            int toplam = 0;
-            while (reqem > 0)
-            {
-                say = reqem % 10;
-                toplam = say + toplam;
-                reqem = reqem / 10;
-            }
-            Console.WriteLine(toplam);
+            Employee empl = new Employee("Vusal", "Ahmadzada", "developer", (decimal)5000);
+
+
+               Console.WriteLine("Employee Name: {0}", empl.Name);
+            Console.WriteLine("Employee Surname: {0}", empl.Surname);
+            Console.WriteLine("Employee Positon: {0}", empl.Positon);
+            Console.WriteLine("Employee DailySalary: {0:C}", empl.DailySalary);
+
+            Console.ReadLine();
 
         }
     }
-}
-  
-
-
-
-
-int season = 6;
-    switch (season)
-    {
-    case 1:
-    case 2:
-            Console.WriteLine(yaz);
-            break:
-        case 3:
-    case 4:
-        Console.WriteLine(yay);
-        break:
-        case 5:
-        Console.WiteLine(payiz);
-        break:
-        case 6:
-        Console.WriteLine(qis);
-    default:
-        Console.WriteLine("duzgun daxil ediniz");
-        break;
-    }
-
-
-
-
-
-
-int = eded;
-int i;
-if (eded=0)
-{ Console.WriteLine("ne sadedir nede murekkebdir"); }
-else if (eded < 0)
-{ Console.WriteLine("musbet eded daxil edin"); }
-else
-{
-    while(i<eded)
-    {
-        if (eded % i ==0)
-        {
-            bolen++;
-            i++;
-        }
-        else
-        {
-            i++;
-        }
-    }
-}
-if (bolen >2)
-{ Console.WriteLine("murekkeb ededdir"); }
-else
-{
-    Console.WriteLine("sade ededdir");
 }
