25c30bf7d98f964dc578891cb39ca8899b196bd6
VusalAhmed <94452551+VusalAhmed@users.noreply.github.com>
2021-12-05 23:00:04 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,81 @@
-﻿using System;
+﻿using ImTools;
+using System;
 
-namespace Employee
+namespace stringreverse1
 {
     class Program
     {
+        private static int i;
+
         static void Main(string[] args)
-         
         {
-            Employee empl = new Employee("Vusal", "Ahmadzada", "developer", (decimal)5000);
-
-
-               Console.WriteLine("Employee Name: {0}", empl.Name);
-            Console.WriteLine("Employee Surname: {0}", empl.Surname);
-            Console.WriteLine("Employee Positon: {0}", empl.Positon);
-            Console.WriteLine("Employee DailySalary: {0:C}", empl.DailySalary);
-
-            Console.ReadLine();
-
+            
+            string str;
+            string result = "";
+            Console.WriteLine("Orginal: Hello World");
+            str = Console.ReadLine();
+            for (int i = str.Length - 1; i >= 0; --i) ;
+            result = result + str[i];
+            Console.WriteLine("Reverse String:" + result);
         }
+    }
+}
+
+
+
+ 
+public class StudentAndTeacherTest
+{
+    public static void Main()
+    {
+        Person myPerson = new Person();
+        myPerson.Greet();
+
+        Student myStudent = new Student();
+        myStudent.SetAge(20);
+        myStudent.Greet();
+        myStudent.ShowAge();
+        myStudent.Study();
+
+        Teacher myTeacher = new Teacher();
+        myTeacher.SetAge(28);
+        myTeacher.Greet();
+        myTeacher.Explain();
+    }
+}
+
+public class Person
+{
+    protected int age;
+
+    public void Greet()
+    {
+        Console.WriteLine("Hello!");
+    }
+
+    public void SetAge(int x)
+    {
+        age = x; 
+    }
+}
+
+public class Student : Person
+{
+    public void Study()
+    {
+        Console.WriteLine("I'm studying");
+    }
+
+    public void ShowAge()
+    {
+        Console.WriteLine("My age is {0} years old", age);
+    }
+}
+
+public class Teacher : Person
+{
+    public void Explain()
+    {
+        Console.WriteLine("I'm explaining");
     }
 }
